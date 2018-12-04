2a5abab301c16ba49f32049db733f4a11caa1c15
vmcasbarro <vincenzo.casbarro@gmail.com>
2018-12-04 10:14:25 -0500

    render map in StopMap component
    - currently hard-coding in the address parameter because of this.props issue...

--- src/stops/components/StopMap.js
+++ src/stops/components/StopMap.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
-import { withRouter, Link } from 'react-router-dom'
+import { withRouter} from 'react-router-dom'
 
-import { handleErrors, createTrip, getTrips, deleteTrip } from '../api'
 import messages from '../messages'
 import apiUrl from '../../apiConfig'
+
+// google maps key and wrapper
+import { googleMapsApiKey } from '../../.env.js'
+const loadGoogleMapsApi = require('load-google-maps-api')
+
 
 class StopMap extends Component {
   constructor (props) {
@@ -14,25 +18,184 @@ class StopMap extends Component {
     }
 
     // This binding is necessary to make `this` work in the callback
-    // this.onDeleteTrip = this.onDeleteTrip.bind(this)
+    // this.componentDidMount = this.componentDidMount.bind(this)
 
   }
 
   componentDidMount() {
+    // destructure stop passed in as a prop from Stop
+    const { stop } = this.props
+    // this returns an empty object, not sure why...
+    console.log('what stop?', stop)
+
+    // initialize map variable which will be the instance of google map
+    let map
+
+    // load google Maps API, which returns a promise
+    loadGoogleMapsApi({key: googleMapsApiKey})
+      .then((googleMaps) => {
+
+        map = new googleMaps.Map(document.querySelector('.map'), {
+          center: {
+            lat: 13.434811,
+            lng: 103.889144
+          },
+          zoom: 13,
+          styles: [
+            {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+            {
+              featureType: 'administrative',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#c9b2a6'}]
+            },
+            {
+              featureType: 'administrative.land_parcel',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#dcd2be'}]
+            },
+            {
+              featureType: 'administrative.land_parcel',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#ae9e90'}]
+            },
+            {
+              featureType: 'landscape.natural',
+              elementType: 'geometry',
+              stylers: [{color: '#dfd2ae'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'geometry',
+              stylers: [{color: '#dfd2ae'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#93817c'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry.fill',
+              stylers: [{color: '#a5b076'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#447530'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#f5f1e6'}]
+            },
+            {
+              featureType: 'road.arterial',
+              elementType: 'geometry',
+              stylers: [{color: '#fdfcf8'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#f8c967'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#e9bc62'}]
+            },
+            {
+              featureType: 'road.highway.controlled_access',
+              elementType: 'geometry',
+              stylers: [{color: '#e98d58'}]
+            },
+            {
+              featureType: 'road.highway.controlled_access',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#db8555'}]
+            },
+            {
+              featureType: 'road.local',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#806b63'}]
+            },
+            {
+              featureType: 'transit.line',
+              elementType: 'geometry',
+              stylers: [{color: '#dfd2ae'}]
+            },
+            {
+              featureType: 'transit.line',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#8f7d77'}]
+            },
+            {
+              featureType: 'transit.line',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#ebe3cd'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'geometry',
+              stylers: [{color: '#dfd2ae'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry.fill',
+              stylers: [{color: '#b9d3c2'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#92998d'}]
+            }
+          ]
+        })
+
+        const geocoder = new googleMaps.Geocoder()
+        const address = stop.location
+
+        geocoder.geocode( { 'address': 'New York'}, function(results, status) {
+          if (status == 'OK') {
+            map.setCenter(results[0].geometry.location)
+            console.log(results[0].geometry.location.lat())
+            console.log(results[0].geometry.location.lng())
+            const marker = new google.maps.Marker({
+              position: results[0].geometry.location,
+              map: map,
+              title: 'Hello World!'
+            })
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status)
+          }
+        })
+
+
+      })
+
   }
 
   render () {
+    const styles = {
+      height: '500px'
+    }
 
-    const { title, stop } = this.props
+    const { title } = this.state
+
+
 
     return(
       <React.Fragment>
 
         <h1>{title}</h1>
+        <div className="map" style={styles}></div>
+
+
 
       </React.Fragment>
     )
   }
 }
 
-export default StopMap
+export default withRouter(StopMap)
