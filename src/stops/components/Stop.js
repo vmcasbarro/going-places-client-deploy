5b1d653146e021981befb63f12ab4191683baabf
vmcasbarro <vincenzo.casbarro@gmail.com>
2018-12-03 21:33:14 -0500

    add chart and dummy data to stop component!
    - need to find a way to update chartData after getting highs and lows from dark sky

--- src/stops/components/Stop.js
+++ src/stops/components/Stop.js
@@ -4,10 +4,9 @@ import { withRouter, Link } from 'react-router-dom'
 import { handleErrors, getForecast } from '../api'
 import messages from '../messages'
 import apiUrl from '../../apiConfig'
-import { googleMapsApiKey, darkSkyApiKey } from '../../.env.js'
+import { googleMapsApiKey } from '../../.env.js'
 const googleTranslate = require('google-translate')(googleMapsApiKey)
-const DarkSky = require('dark-sky')
-const darksky = new DarkSky(darkSkyApiKey)
+const LineChart = require('react-chartjs').Line
 
 class Stop extends Component {
   constructor (props) {
@@ -21,7 +20,9 @@ class Stop extends Component {
       translations: [],
       stop: {},
       stopNumber: null,
-      weatherData: {}
+      weatherData: {},
+      dailyHighs: [],
+      dailyLows: []
     }
 
 
@@ -29,10 +30,101 @@ class Stop extends Component {
     // This binding is necessary to make `this` work in the callback
     this.onTranslate = this.onTranslate.bind(this)
     this.updateTranslation = this.updateTranslation.bind(this)
-    // this.onGetTrip = this.onGetTrip.bind(this)
+    this.getTemps = this.getTemps.bind(this)
+
 
     // get STOP id which is used by most of the methods in this class
     this.id = this.props.match.params.id
+
+  }
+
+  chartData = {
+    labels: ['today', '+1', '+2', '+3', '+4', '+5', '+6', '+7'],
+    datasets: [
+      {
+        label: 'Daily High Temp',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: [65, 59, 0, 80, 81, 56, 55, 40]
+      },
+      {
+        label: 'Daily Low Temp',
+        fillColor: 'rgba(151,187,205,0.2)',
+        strokeColor: 'rgba(151,187,205,1)',
+        pointColor: 'rgba(151,187,205,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(151,187,205,1)',
+        data: [28, 48, 0, 40, 19, 86, 27, 90]
+      }
+    ]
+  }
+
+  getTemps () {
+    const { daily: {data} = {}} = this.state.weatherData
+    console.log(data)
+    const dailyHighs = data.map((day) => {
+      return day.temperatureHigh
+    })
+    const dailyLows = data.map((day) => {
+      return day.temperatureLow
+    })
+    this.setState({dailyHighs: dailyHighs})
+    this.setState({dailyLows: dailyLows})
+    // console.log('highs', dailyHighs)
+    // console.log('lows', dailyLows)
+  }
+
+  chartOptions = {
+
+  	///Boolean - Whether grid lines are shown across the chart
+  	scaleShowGridLines : true,
+
+  	//String - Colour of the grid lines
+  	scaleGridLineColor : 'rgba(0,0,0,.05)',
+
+  	//Number - Width of the grid lines
+  	scaleGridLineWidth : 1,
+
+  	//Boolean - Whether to show horizontal lines (except X axis)
+  	scaleShowHorizontalLines: true,
+
+  	//Boolean - Whether to show vertical lines (except Y axis)
+  	scaleShowVerticalLines: true,
+
+  	//Boolean - Whether the line is curved between points
+  	bezierCurve : true,
+
+  	//Number - Tension of the bezier curve between points
+  	bezierCurveTension : 0.4,
+
+  	//Boolean - Whether to show a dot for each point
+  	pointDot : true,
+
+  	//Number - Radius of each point dot in pixels
+  	pointDotRadius : 4,
+
+  	//Number - Pixel width of point dot stroke
+  	pointDotStrokeWidth : 1,
+
+  	//Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+  	pointHitDetectionRadius : 20,
+
+  	//Boolean - Whether to show a stroke for datasets
+  	datasetStroke : true,
+
+  	//Number - Pixel width of dataset stroke
+  	datasetStrokeWidth : 2,
+
+  	//Boolean - Whether to fill the dataset with a colour
+  	datasetFill : true,
+
+  	//Boolean - Whether to horizontally center the label and point dot inside the grid
+  	offsetGridLines : false
   }
 
   onGetForecast(lat, long) {
@@ -43,6 +135,7 @@ class Stop extends Component {
       .then(handleErrors)
       .then(response => response.json())
       .then((JSONresponse) => {this.setState({ weatherData: JSONresponse })})
+      .then(() => this.getTemps())
       .catch(() => flash(messages.apiFailure, 'flash-error'))
   }
 
@@ -136,7 +229,14 @@ class Stop extends Component {
               </form>
               <div className="translation-response" >{translation.translatedText}</div>
             </div>
-            <div className="col-md-6" style={weatherDiv}>weather</div>
+            <div className="col-md-6" style={weatherDiv}>
+              <LineChart
+                data={this.chartData}
+                options={this.chartOptions}
+                width="500"
+                height="250"
+              />
+            </div>
           </div>
           <div className="row">
             <div className="col-12" style={mapDiv}>map</div>
