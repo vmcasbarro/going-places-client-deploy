cb274a61a940765672020ed44f944ad482f0147b
vmcasbarro <vincenzo.casbarro@gmail.com>
2018-12-05 09:57:51 -0500

    GOOGLE VISION TRANSLATION IS WORKING!
    - happy commit
    - what this component does:
      1. get user-provided file (an image)
      2. converts image file to base64 string
      3. display thumbnail preview using base64 string
      4. remove prefix from base64 for Google Vision
      5. make Google Vision api call, requesting text detection
      6. take 'summary' of api call response, which is html text, add to state
      7. take 'locale' of api call response, which is Vision's best guess at language/country of the text, add to state (not currently doing anything with this, but may be useful for future versions)
      8. display 'summary' text to the user
      9. send 'summary' text to Google Translate API, ask for english translation
      10. update state once translated text comes back
      11. display translated text to the user

--- src/stops/components/VisionTranslate.js
+++ src/stops/components/VisionTranslate.js
@@ -4,16 +4,17 @@ import { withRouter} from 'react-router-dom'
 import messages from '../messages'
 import apiUrl from '../../apiConfig'
 
-// google maps key and wrapper
-import { googleMapsApiKey } from '../../.env.js'
-const loadGoogleMapsApi = require('load-google-maps-api')
-
+// npm module for sanitizing user imput
 import DOMPurify from 'dompurify'
 
 // node module for converting image to base64 string
 import FileBase64 from 'react-file-base64'
 
 import { handleErrors, getText } from '../api'
+
+// translation imports
+import { googleMapsApiKey } from '../../.env.js'
+const googleTranslate = require('google-translate')(googleMapsApiKey)
 
 class VisionTranslate extends Component {
   constructor (props) {
@@ -24,7 +25,8 @@ class VisionTranslate extends Component {
       brandNewFile: {},
       visionResponse: {
         description: ''
-      }
+      },
+      descriptionTranslated: ''
 
     }
 
@@ -58,7 +60,34 @@ class VisionTranslate extends Component {
         this.setState({ visionResponse: jsonResponse.responses[0].textAnnotations[0]
         })
       })
+      .then(() => this.translate())
       .catch(() => flash(messages.getTextFailure, 'flash-error'))
+  }
+
+  translate() {
+    const { description } = this.state.visionResponse
+    const { descriptionTranslated } = this.state
+
+    // promisify googleTranslate API call
+    const transPromise = function (text, language) {
+      return new Promise((resolve, reject) => {
+        googleTranslate.translate(text, language, (err, data) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(data)
+          }
+        })
+      })
+    }
+
+    transPromise(description, 'en')
+      .then((data) => {this.addTranslation(data.translatedText)})
+      .catch(console.error)
+  }
+
+  addTranslation = translatedText => {
+    this.setState({ descriptionTranslated: translatedText })
   }
 
   // Callback
@@ -73,6 +102,7 @@ class VisionTranslate extends Component {
 
   render () {
     const { description } = this.state.visionResponse
+    const { descriptionTranslated } = this.state
     const { base64 } = this.state.newFile
 
     const content = {
@@ -90,7 +120,14 @@ class VisionTranslate extends Component {
           multiple={ false }
           onDone={ this.getFile.bind(this) } />
         <button type="submit" onClick={this.onGetText}>get text</button>
-        <div style={content}>{DOMPurify.sanitize(description)}</div>
+        <div style={content}>
+          {description && <span> <code>detected text:</code> </span> }
+          {DOMPurify.sanitize(description)}
+        </div>
+        <div style={content}>
+          {description && <span> <code>translated text:</code> </span> }
+          {DOMPurify.sanitize(descriptionTranslated)}
+        </div>
         <div style={thumbnailSize}>
           { base64 && <img src={base64} className="img-thumbnail" alt="image preview" /> }
         </div>
