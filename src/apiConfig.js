746f87abbbc935b7a96523b43ab427ef303bc31b
vmcasbarro <vincenzo.casbarro@gmail.com>
2018-12-05 10:32:55 -0500

    change deploy scripts & update deployed API url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://shielded-castle-72591.herokuapp.com/',
+  production: 'https://shielded-castle-72591.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
